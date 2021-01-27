d52a0660e715075f1710bf40d7d86a4242fc8f57
kd Bromley <kd.bromley1@gmail.com>
2021-01-27 10:55:22 -0800

    add auth token validation, filters

--- server.js
+++ server.js
@@ -1,13 +1,47 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const MOVIEDEX = require('./movidex.json');
 
 const app = express();
 
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-    res.send('Hello, world!');
-});
+app.use(function validateToken(req, res, next)  {
+    const apiToken = process.env.API_TOKEN;
+    const bearerToken = req.get('Authorization');
+    console.log(`validating token`);
+    if (!bearerToken || bearerToken.split(' ')[1] !== apiToken ) {
+        return res
+            .status(401)
+            .json({ error: 'Unauthorized request'});
+    };
+    next();
+})
+
+app.get('/movies', function getMovies(req, res) {
+    let response = MOVIEDEX;
+    const { genre, country, rating } = req.query;
+
+    if(genre) {
+        response = response.filter(movie => 
+            movie.genre.toLowerCase().includes(genre.toLowerCase()) 
+        )
+    }
+
+    if(country) {
+        response = response.filter(movie => 
+            movie.country.toLowerCase().includes(country.toLowerCase())
+        )
+    }
+
+    if(rating) {   
+        response = response.filter(movie => 
+            Number(movie.avg_vote) >= Number(rating)
+        ) 
+    }
+    res.json(response)
+})
 
 const PORT = 8000;
 
